e909511839235bb0a6a374553e9d1f9c4a52b84a
imano <imano@10.254.60.191>
2021-02-04 16:54:30 +0100

    versión inicial

---
+++ src/MiPrimeraClase.java
@@ -0,0 +1,13 @@
+
+public class MiPrimeraClase {
+
+	public static void main(String[] args) {
+		System.out.println("esto es un mensaje");
+		otroMetodo();
+
+	}
+	private static void otroMetodo() {
+		System.out.println("otro metodo");
+	}
+
+}
